eced4f727f65e36fc5d5b39bd57c5f4ad02cf856
Nisugi <97870955+Nisugi@users.noreply.github.com>
2025-03-20 07:50:37 -0500

    spectral_triton_protector.rb

---
+++ spectral_triton_protector.rb
@@ -0,0 +1,85 @@
+:noun               => 'protector'
+:name               => 'spectral triton brawler'
+:level              => 98
+:size               => 'medium'
+:height             => 6
+:family             => 'triton'
+:body               => 'biped'
+:type               => ['boss', 'non-corporeal', 'undead']
+:areas              => ['the Atoll']
+:right_hand         => 'coral-hilted heavy ball and chain'
+:left_hand          => 'bronze-bound driftwood greatshield'
+:armor              => 'full plate (natural)'
+
+:skin               => []
+:require_blunt      => false
+:coins              => true
+:gems               => true
+:magic_items        => true
+:boxes              => true
+:other              => nil
+
+:legged             => true
+:bones              => false
+:blood              => false
+:flying             => false
+:shield             => true
+:deflects           => true
+:hides              => false
+:disarms            => false
+:casts              => false
+:melee              => true
+:ucs                => false
+
+:hp                 => 300
+:melee_as           => (420..424)
+:bolt_as            => nil
+:melee_ds           => (274..390)
+:ranged_ds          => (264..389)
+:bolt_ds            => (273..428)
+:udf                => (382..628)
+:uac                => nil
+:cs                 => [
+  :bard             => nil
+  :cleric           => nil
+  :empath           => nil
+  :paladin          => nil
+  :ranger           => nil
+  :sorcerer         => nil
+  :wizard           => nil
+  :minor_elemental  => nil
+  :major_elemental  => nil
+  :minor_spiritual  => nil
+  :major_spiritual  => nil
+  :minor_mental     => nil
+  :major_mental     => nil
+]
+:td                 => [
+  :bard             => 390
+  :cleric           => nil
+  :empath           => nil
+  :paladin          => nil
+  :ranger           => (300..337)
+  :sorcerer         => 410
+  :wizard           => nil
+  :minor_elemental  => 435
+  :major_elemental  => nil
+  :minor_spiritual  => (388..393)
+  :major_spiritual  => nil
+  :minor_mental     => nil
+  :major_mental     => nil
+]
+
+:resists            => []
+:suscepts           => []
+:attacks            => ['dizzying swing', 'mstrike', 'hurl']
+:cmans              => []
+:special_abilities  => []
+:spells_cast        => []
+:spells_worn        => []
+
+:arrival            => /^A spectral triton protector rushes in with powerful strides, (?:her|his) slender tail flickering behind (?:her|him)!$/
+:enter              => /^A spectral triton protector just arrived.$/
+:exit               => /^A spectral triton protector heads \w+.$/
+:description        => /^Coral-spiked pauldrons are draped across a lanky triton protector's shoulders, the leather straps taut across (?:her|his) bare chest, the hide cutting into (?:her|his) tattooed blue-green flesh.  Thick rings of ivory encircle (?:her|his) forearms and calves, etched in crude runes caked with blackish mud.  Uneven streaks of pigment decorate (?:her|his) sunken cheekbones, the remnants splattered across (?:her|his) trident-inked collarbones.$/
+:death_regex        => /^The spectral form of the triton protector tenses in agony as (?:he|she) begins to dissolve from the bottom up!$/
